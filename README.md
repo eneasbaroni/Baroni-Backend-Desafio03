e0ad2a14e3a9509939224fee089351ad741e9fd4
Eneas Baroni <eneasbaroni@gmail.com>
2022-07-09 16:15:04 -0300

    Desafio 03

---
+++ README.md
@@ -0,0 +1,6 @@
+# BARONI Eneas - Curso CODERHOUSE Backend
+
+Desafio 03
+
+Link Glitch
+https://island-jet-painter.glitch.me
---
+++ app.js
@@ -0,0 +1,150 @@
+const fs = require('fs');
+
+const express = require("express");
+const app = express();
+const port = 8080;
+
+//Logica de la Class
+class Contenedor {
+  constructor (nombre) {
+    this.nombre = nombre;    
+  }
+
+  //funcion para leer el archivo
+  async read () {
+    try {      
+      const data = await fs.promises.readFile(`./${this.nombre}.txt`, `utf-8`)
+      const dataObj = JSON.parse(data)       
+      return (dataObj)
+    }
+    catch (err) {      
+      throw new Error('No se pudo leer archivo', err)
+    }
+  }
+
+  //funcion para detectar el ID mas grande
+  lastID = (arr) => {  
+    let id = 0;
+    if (arr.length > 0) {
+      for (const el of arr ) {
+        if (el.id > id) {
+          id = el.id
+        }
+      }
+    }
+    return id
+  } 
+
+  //funcion para guardar un objeto en el archivo
+  async save(object) {
+      
+    try {             
+      const dataObj = await this.read()
+      const previusID = await this.lastID(dataObj)
+      const newObject = {...object, id:(previusID + 1 )}
+      console.log(newObject)  
+
+      //agregar objeto al array  (PORQUEEEEEEE NO PUEDO USAR PUSSSSHHHHHH????????)
+      let newArr =  dataObj.concat(newObject) 
+
+      //reescribo el archivo con el nuevo objeto
+      await fs.promises.writeFile(`./${this.nombre}.txt`, JSON.stringify(newArr, null, 2))
+      console.log('EXITO')
+      return(previusID + 1)
+    }
+    catch (err) {      
+      throw new Error('Error de escritura', err)
+    }    
+  }    
+
+  //funcion para obtener objeto segun ID
+  async getById(x) {
+    
+    try {             
+      const dataObj = await this.read()      
+      const newObject = dataObj.filter(el => el.id === x)
+      return newObject[0]
+    }
+    catch (err) {      
+      throw new Error('Error de Lectura', err)
+    } 
+
+  }
+
+  //funcion que devuelve todos los objetos
+  async getAll() {
+    try {             
+      const dataObj = await this.read()  
+      return dataObj
+    }
+    catch (err) {      
+      throw new Error('Error de Lectura', err)
+    }
+  }
+
+  //funcion que elimina segun id
+  async deleteById(x) {      
+    try {                   
+      const dataObj = await this.read()
+      const newArr = dataObj.filter(el => el.id !== x)       
+      //reescribo el archivo con el nuevo objeto
+      await fs.promises.writeFile(`./${this.nombre}.txt`, JSON.stringify(newArr, null, 2))
+    }
+    catch (err) {    
+      throw new Error('Error de escritura', err)
+    }    
+  } 
+
+  //funcion que elimina todos los objetos
+  async deleteAll() {      
+    try { 
+      const newArr = [] 
+      //reescribo el archivo vacio
+      await fs.promises.writeFile(`./${this.nombre}.txt`, JSON.stringify(newArr, null, 2))
+    }
+    catch (err) {      
+      throw new Error('Error de escritura', err)
+    }    
+  } 
+  
+} 
+
+//delcaro un nuevo objeto a artir de la class
+const productos = new Contenedor("productos");
+
+//Logica del servidor
+app.get("/", (req, res) => {
+
+  res.send('<h1>Bienvenidos al servidor express</h1>');
+  
+});
+
+app.get("/productos", (req, res) => {
+
+  productos.getAll()
+
+  .then (data => data.map (function (el) {
+    return el.title
+  }))
+
+  .then (data => res.send(data))
+
+});
+
+app.get("/productoRandom", (req, res) => {
+
+  let x = Math.floor(Math.random()*3)+1;
+
+  productos.getById(x)
+
+  .then (data => res.send(data)) 
+  
+});
+
+const server = app.listen(port, () => {
+  console.log(`Servidor http escuchando en el puerto ${port}`);
+});
+server.on("error", (error) => console.log(`Error en servidor ${error}`));
+
+
+
